0d1178ba6e56cf7d0407432c1d966ae591ffebe5
shavez94 <shavezazmi.azmi@gmail.com>
2019-12-09 22:29:30 +0530

    final

--- BOOMKAR/Assets/unityads.cs
+++ BOOMKAR/Assets/unityads.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
+
 
 
 public class unityads : MonoBehaviour {
 
+    int timer;
+    
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
